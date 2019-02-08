8975cfad19f43fea5d7b39edad068380ecea25d6
maccracken <robert.maccracken@gmail.com>
2019-02-08 11:40:46 -0800

    try again

--- spec/blkbx_spec.rb
+++ spec/blkbx_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'VERSION' do
 end
 
 url  = 'https://www.google.com/'
-args = %w[no-sandbox headless disable-gpu]
+args = %w[no-sandbox disable-gpu disable-dev-shm-usage]
 test_browsers = %I[firefox chrome]
 test_browsers << :safari if OS.mac? == true
 test_browsers << %I[ie edge] if OS.windows? == true
