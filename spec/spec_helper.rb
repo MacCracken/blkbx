4d4d9db0624be798303a282e144ed69c3df717d3
maccracken <robert.maccracken@gmail.com>
2019-02-11 11:07:17 -0800

    fix to gem name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'bundler/setup'
 require 'blkbx'
 require 'os'
 require 'webdrivers'
-require 'selenium-webdrivers'
+require 'selenium-webdriver'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
