f7594ceba159ef44062d396465d645cb834979ed
maccracken <robert.maccracken@gmail.com>
2019-02-11 09:35:27 -0800

    spec update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 require 'blkbx'
 require 'os'
-# require 'webdrivers'
+require 'webdrivers'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
