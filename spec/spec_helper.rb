56ab2ffc96035867b27655af86173bed76846f23
maccracken <robert.maccracken@gmail.com>
2019-02-11 11:14:27 -0800

    removal of selenium-webdrivers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@ require 'bundler/setup'
 require 'blkbx'
 require 'os'
 require 'webdrivers'
-require 'selenium-webdriver'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
